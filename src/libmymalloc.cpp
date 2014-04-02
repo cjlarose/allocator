eb9032b7695a723c8a45958a4212d06bb3c5a87f
Chris LaRose <cjlarose@gmail.com>
2014-04-01 20:26:16 -0700

    Add libmymalloc source

---
+++ src/libmymalloc.cpp
@@ -0,0 +1,40 @@
+#include "allocator.hpp"
+#include "sequential_allocator.hpp"
+#include "parallel_allocator.hpp"
+#include "libmymalloc.h"
+
+#ifdef PROG1
+SequentialAllocator<false> *alloc;
+int myInit(int num_cores) {
+    auto myalloc = SequentialAllocator<false>();
+#endif
+
+#ifdef PROG2
+SequentialAllocator<true> *alloc;
+int myInit(int num_cores) {
+    auto myalloc = SequentialAllocator<true>();
+#endif
+
+#ifdef PROG3
+ParallelAllocator<false> *alloc;
+int myInit(int num_cores) {
+    auto myalloc = ParallelAllocator<false>(num_cores);
+#endif
+
+#ifdef PROG4
+ParallelAllocator<true> *alloc;
+int myInit(int num_cores) {
+    auto myalloc = ParallelAllocator<true>(num_cores);
+#endif
+
+    alloc = &myalloc;
+    return 0;
+}
+
+void *myMalloc(int size) {
+    return alloc->malloc(size);
+}
+
+void myFree(void *ptr) {
+    alloc->free(ptr);
+}
