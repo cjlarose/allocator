96cce493d6fe65b2a10a5767603b0bdd859e18a8
Chris LaRose <cjlarose@gmail.com>
2014-04-12 09:45:26 -0700

    Free list initialization

--- include/sequential_allocator.hpp
+++ include/sequential_allocator.hpp
@@ -2,7 +2,6 @@
 #define _SEQ_ALLOC_HPP_ 
 
 #include <cstddef>
-#include <mutex>
 #include "allocator.hpp"
 
 template<std::size_t block_size>
@@ -12,18 +11,34 @@ struct FreeListNode {
     char data[block_size];
 };
 
+const int NUM_SMALL_BLOCKS = 4096;
+const int NUM_LARGE_BLOCKS = 256;
+
+const std::size_t SMALL_BLOCK_SZ = 64;
+const std::size_t LARGE_BLOCK_SZ = 1024;
+
 class SequentialAllocator: public Allocator {
     public:
         SequentialAllocator();
         void *malloc(int size);
         void free(void *ptr);
     private:
-        FreeListNode<64> small_list[4096];
-        FreeListNode<1024> large_list[256];
-        //std::mutex mutex;
+        FreeListNode<SMALL_BLOCK_SZ> small_list[NUM_SMALL_BLOCKS];
+        FreeListNode<LARGE_BLOCK_SZ> large_list[NUM_LARGE_BLOCKS];
 };
 
 SequentialAllocator::SequentialAllocator() {
+    for (int i = 0; i < NUM_SMALL_BLOCKS; ++i) {
+        small_list[i].size = SMALL_BLOCK_SZ;
+        small_list[i].next = &small_list[i+1];
+    }
+    small_list[NUM_SMALL_BLOCKS - 1].next = NULL;
+
+    for (int i = 0; i < NUM_LARGE_BLOCKS; ++i) {
+        large_list[i].size = LARGE_BLOCK_SZ;
+        large_list[i].next = &large_list[i+1];
+    }
+    large_list[NUM_LARGE_BLOCKS - 1].next = NULL;
 }
 
 void *SequentialAllocator::malloc(int size) {
