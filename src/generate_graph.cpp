700b63cadbc94c308391f1fbb5bdc2cb4a009f84
Chris LaRose <cjlarose@gmail.com>
2014-04-14 08:48:01 +0000

    Add sequential calculation for graphs

--- src/generate_graph.cpp
+++ src/generate_graph.cpp
@@ -27,7 +27,7 @@ void *allocate(void *data) {
     Allocator *alloc = (Allocator *) data;
     void* ptrs[SM_BLOCKS_PER_THREAD + LG_BLOCKS_PER_THREAD];
 
-    for (int round = 0; round < 1024; ++round) {
+    for (int round = 0; round < 16384; ++round) {
         int i = 0;
         for (; i < SM_BLOCKS_PER_THREAD; ++i) {
             ptrs[i] = alloc->malloc(64);
@@ -43,9 +43,23 @@ void *allocate(void *data) {
             alloc->free(ptrs[j]);
         }
     }
+
 }
 
-void test_alloc(Allocator *alloc, int p) {
+typedef struct {
+    Allocator *alloc;
+    int p;
+} thread_data_t;
+
+void *allocate_p(void *data) {
+    thread_data_t *t_data = (thread_data_t *) data;
+    int p = t_data->p;
+    Allocator *alloc = t_data->alloc;
+    for (int i = 0; i < p; ++i)
+        allocate(alloc);
+}
+
+void test_par_alloc(Allocator *alloc, int p) {
     pthread_t *threads = (pthread_t *) malloc(sizeof(pthread_t) * p);
     for (int i = 0; i < p; ++i) {
         pthread_create(&threads[i], NULL, allocate, alloc);
@@ -55,10 +69,39 @@ void test_alloc(Allocator *alloc, int p) {
     }
 }
 
-struct timeval time_alloc(Allocator *alloc, int p) {
+struct timeval time_par_alloc(Allocator *alloc, int p) {
     struct timeval start, stop, elapsed;
     gettimeofday(&start, NULL);
-    test_alloc(alloc, p);
+    test_par_alloc(alloc, p);
+    gettimeofday(&stop, NULL);
+    elapsed.tv_sec = stop.tv_sec - start.tv_sec;
+    elapsed.tv_usec = stop.tv_usec - start.tv_usec;
+    return elapsed;
+}
+
+void test_seq_alloc(Allocator *alloc, int p) {
+    pthread_t *threads = (pthread_t *) malloc(sizeof(pthread_t) * p);
+
+    thread_data_t data;
+    data.alloc = alloc;
+    data.p = p;
+
+    thread_data_t no_work_data;
+    no_work_data.alloc = alloc;
+    no_work_data.p = 0;
+
+    for (int i = 0; i < p; ++i) {
+        pthread_create(&threads[i], NULL, allocate_p, i == 0 ? &data : &no_work_data);
+    }
+    for (int i = 0; i < p; ++i) {
+        pthread_join(threads[i], NULL);
+    }
+}
+
+struct timeval time_seq_alloc(Allocator *alloc, int p) {
+    struct timeval start, stop, elapsed;
+    gettimeofday(&start, NULL);
+    test_seq_alloc(alloc, p);
     gettimeofday(&stop, NULL);
     elapsed.tv_sec = stop.tv_sec - start.tv_sec;
     elapsed.tv_usec = stop.tv_usec - start.tv_usec;
@@ -67,39 +110,29 @@ struct timeval time_alloc(Allocator *alloc, int p) {
 
 int main() {
     Allocator *alloc;
+    std::cout << "Program 1: Sequential\n";
+    for (int p = 1; p <= 8; ++p) {
+        alloc = new SequentialAllocator<2048, 128>();
+        struct timeval elapsed = time_seq_alloc(alloc, p);
+        double elapsed_d = elapsed.tv_sec + elapsed.tv_usec / 1000000.0;
+        std::cout << "(" << p << ", " << elapsed_d << ")\n";
+    }
 
     std::cout << "Program 2: Synchronized\n";
     for (int p = 1; p <= 8; ++p) {
         alloc = new SynchronizedSequentialAllocator<2048 * 8, 128 * 8>();
-        struct timeval elapsed = time_alloc(alloc, p);
+        struct timeval elapsed = time_par_alloc(alloc, p);
+        double elapsed_d = elapsed.tv_sec + elapsed.tv_usec / 1000000.0;
+        std::cout << "(" << p << ", " << elapsed_d << ")\n";
+    }
+
+    std::cout << "Program 3: Local Lists\n";
+    for (int p = 1; p <= 8; ++p) {
+        alloc = new ParallelAllocator(p);
+        struct timeval elapsed = time_par_alloc(alloc, p);
         double elapsed_d = elapsed.tv_sec + elapsed.tv_usec / 1000000.0;
         std::cout << "(" << p << ", " << elapsed_d << ")\n";
     }
 
     return 0;
 }
-/*
-int main() {
-    myInit(NUM_THREADS);
-    void* ptrs[NUM_THREADS * BLOCKS_PER_THREAD];
-    int i;
-
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM);
-    pthread_t threads[NUM_THREADS];
-    struct thread_var vars[NUM_THREADS];
-    for (i = 0; i < NUM_THREADS; i++) {
-        struct thread_var* var = &vars[i];
-        var->offset = BLOCKS_PER_THREAD * i;
-        var->amount = BLOCKS_PER_THREAD;
-        var->ptrs = ptrs;
-        pthread_create(&threads[i], &attr, allocate, (void*) var);
-    }
-    for (i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
-    }
-    
-    return 0;
-}
-*/
